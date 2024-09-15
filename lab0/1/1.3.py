9681fb0ea7418a83b31af60713cadf652f710be5
MarinaZabotkina <zabotkina06@mail.ru>
2024-09-15 23:21:47 +0300

    Update 1.3.py

--- lab0/1/1.3.py
+++ lab0/1/1.3.py
@@ -3,7 +3,10 @@ a, b = map(int, f.readline().split())
 f.close()
 
 f = open('output.txt', 'w')
-f.write(str(a + b))
+if (-10**9 <= a <= 10**9) and (-10**9 <= b <= 10**9):
+    f.write(str(a + b))
+else:
+    print('Числа не подходят под условие')
 f.close()
 
 
