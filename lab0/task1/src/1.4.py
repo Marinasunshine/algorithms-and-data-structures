ccb92a4f42ba68297d6c6d18df24166a82829014
MarinaZabotkina <zabotkina06@mail.ru>
2024-09-27 17:18:57 +0300

    Update 1.4.py
    Changing the text file path

--- lab0/task1/src/1.4.py
+++ lab0/task1/src/1.4.py
@@ -1,8 +1,8 @@
-f = open('input.txt')
+f = open('../txtf/input.txt')
 a, b = map(int, f.readline().split())
 f.close()
 
-f = open('output.txt', 'w')
+f = open('../txtf/output.txt', 'w')
 if (-10**9 <= a <= 10**9) and (-10**9 <= b <= 10**9):
     f.write(str(a + b**2))
 else:
