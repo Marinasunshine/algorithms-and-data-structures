c030879aeac5e6fc063d79c9b82b149a84bb08b3
Marinasunshine <zabotkina06@mail.ru>
2024-10-14 19:04:56 +0300

    corrected code of task9

--- lab1/task9/src/task9.py
+++ lab1/task9/src/task9.py
@@ -2,8 +2,17 @@ f = open('../txtf/input.txt')
 a, b = f.readline().split()
 f.close()
 
-bin_sum = bin(int(a, 2) + int(b, 2))[2:]
+n = len(a)
+carry = 0
+c = ['0'] * (n + 1)
+
+for i in range(n - 1, -1, -1):
+    sum_bit = int(a[i]) + int(b[i]) + carry
+    c[i + 1] = str(sum_bit % 2)
+    carry = sum_bit // 2
+
+c[0] = str(carry)
 
 f = open('../txtf/output.txt', 'w')
-f.write(bin_sum)
+f.write(''.join(c).lstrip('0') or '0')
 f.close()
